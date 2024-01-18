c8ddeb322829101421d452cb5a5c9851be76ebc5
Aryan Tiwari <63551164+aryantiwari522@users.noreply.github.com>
2024-01-18 14:29:47 +0530

    Time: 7 ms (77.03%), Space: 8.5 MB (71.02%) - LeetHub

---
+++ 0013-roman-to-integer/0013-roman-to-integer.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int romanToInt(string s) {
+        unordered_map<char,int> map;
+        map['I']=1;
+        map['V']=5;
+        map['X']=10;
+        map['L']=50;
+        map['C']=100;
+        map['D']=500;
+        map['M']=1000;
+        int sum=0;
+        for (int i = 0; i < s.length(); i++) {
+            // Check for subtraction cases
+            if (i < s.length() - 1 && map[s[i]] < map[s[i + 1]]) {
+                sum += map[s[i + 1]] - map[s[i]];
+                i++; // Skip the next character since it's already considered
+            } else {
+                sum += map[s[i]];
+            }
+        }
+        return sum;
+    }
+};
