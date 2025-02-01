89463b0d99185cccc698f50bc90139a5addf45e2
Aryan Tiwari <63551164+aryantiwari522@users.noreply.github.com>
2025-02-01 13:16:04 +0530

    Time: 0 ms (100%), Space: 27.7 MB (25%) - LeetHub

--- 3429-special-array-i/3429-special-array-i.cpp
+++ 3429-special-array-i/3429-special-array-i.cpp
@@ -1,8 +1,10 @@
+//now using bits -> taking & with 1 , if even give 0, if odd give 1
+
 class Solution {
 public:
     bool isArraySpecial(vector<int>& nums) {
         for(int i=0;i<nums.size()-1;i++){
-            if(nums[i]%2 == nums[i+1]%2){
+            if((nums[i]&1) == (nums[i+1]&1)){
                 return false;
             }
         }
