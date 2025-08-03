512d756fe3d7c0ed223933dc3f4ec0eed2f8e97b
Aryan Tiwari <63551164+aryantiwari522@users.noreply.github.com>
2025-08-03 12:35:30 +0530

    Time: 32 ms (88.81%), Space: 90.1 MB (60.84%) - LeetHub

---
+++ 2689-rearranging-fruits/2689-rearranging-fruits.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    long long minCost(vector<int>& basket1, vector<int>& basket2) {
+        unordered_map<int,int> mp;
+        int min_Value=INT_MAX;
+        long long result=0;
+        for(int c1:basket1){
+            mp[c1]++;
+            min_Value=min(c1,min_Value);
+        }
+
+        for(int c2:basket2){
+           mp[c2]--;
+           min_Value=min(c2,min_Value);
+        }
+
+        vector<int> v;
+
+        for(auto &it:mp)
+        {
+            int element=it.first;
+            int freq=it.second;
+
+            if(freq==0) continue;
+            if(freq%2 != 0) return -1;
+            for(int i=0;i<abs(freq)/2;i++)
+            {
+                v.push_back(element);
+            }
+        }
+        sort(v.begin(), v.end());
+        for(int i=0;i<v.size()/2;i++)
+        {
+            result+=min(v[i],min_Value*2);
+        }
+        return result;
+    }
+};
