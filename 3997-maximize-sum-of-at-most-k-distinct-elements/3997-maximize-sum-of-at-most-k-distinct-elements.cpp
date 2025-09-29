f6c81f1ae4a3a12ff55c239739f0cc1df5bfb2b2
Aryan Tiwari <63551164+aryantiwari522@users.noreply.github.com>
2025-09-29 23:59:36 +0530

    Time: 0 ms (100%), Space: 26.7 MB (90.9%) - LeetHub

--- 3997-maximize-sum-of-at-most-k-distinct-elements/3997-maximize-sum-of-at-most-k-distinct-elements.cpp
+++ 3997-maximize-sum-of-at-most-k-distinct-elements/3997-maximize-sum-of-at-most-k-distinct-elements.cpp
@@ -6,12 +6,12 @@ public:
         s.push_back(nums[0]);
         int i=1, count=1;
         while(count<k){
+            if(i==nums.size()) break;
             if(nums[i]!=nums[i-1]){
                 s.push_back(nums[i]);
                 count++;
             }
             i++;
-            if(i==nums.size()) break;
         }
         return s;
     }
