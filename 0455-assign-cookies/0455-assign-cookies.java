40326221ad294c20237e99b2f17b72b562e04786
Aryan Tiwari <63551164+aryantiwari522@users.noreply.github.com>
2026-06-15 00:27:55 +0530

    Time: 14 ms (93.68%), Space: 51.5 MB (55.5%) - LeetHub

---
+++ 0455-assign-cookies/0455-assign-cookies.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int findContentChildren(int[] g, int[] s) {
+        Arrays.sort(g);
+        Arrays.sort(s);
+
+        int m=g.length;
+        int n=s.length;
+
+        int i=0,j=0;
+
+        while(i<m && j<n){
+            if(g[i]<=s[j]){
+                i++;
+            }
+            j++;
+        }
+        return i;
+    }
+}
